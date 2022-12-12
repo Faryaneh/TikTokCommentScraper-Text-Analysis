b4cc6831381f6dd24a694b131ec42202fda6ed95
kaypineda <kpine003@odu.edu>
2022-12-12 17:05:16 -0500

    update to only create CSV file

--- src/ScrapeTikTokComments.py
+++ src/ScrapeTikTokComments.py
@@ -8,7 +8,7 @@ from pyperclip import paste, PyperclipException
 from openpyxl import Workbook
 
 cur_dir = path.dirname(path.abspath(__file__))
-csv_path = path.join(cur_dir, "..", "Comments.csv")
+csv_path = path.join(cur_dir, "..", f"Comments_{d.timestamp(d.now())}.csv")
 
 # Needed to initialize the prompt to support ansi escape sequences
 system("")
@@ -28,37 +28,8 @@ except Exception as e:
 	print("\n\x1b[31m[X]\x1b[0m Couldn't write to CSV file. Does it already exist?")
 	sys.exit(1)
 
-
 print("\r\x1b[32m[*]\x1b[0m Writing CSV from clipboard to file + removing carriage return characters ('\\r').")
 
-wb = Workbook()
-ws = wb.active
-
-print("\x1b[34m[*]\x1b[0m Converting CSV file to Excel Workbook (XLSX).", end="", flush=True)
-line_count = 0
-with open(csv_path, 'r+', encoding="utf-8") as f:
-	for row in reader(f):
-		ws.append(row)
-		line_count += 1
-
-print("\r\x1b[32m[*]\x1b[0m Converting CSV file to Excel Workbook (XLSX).")
-
-print(f"\x1b[32m[*]\x1b[0m Written {line_count} line(s).")
-
-print("\x1b[34m[*]\x1b[0m Saving XLSX file.", end="", flush=True)
-
-wb.save(path.join(cur_dir, "..", f"Comments_{d.timestamp(d.now())}.xlsx"))
-
-print("\r\x1b[32m[*]\x1b[0m Saving XLSX file.")
-
-print("\x1b[34m[*]\x1b[0m Deleting CSV file.", end="", flush=True)
-
-print("\r\x1b[34m[*]\x1b[0m Deleting CSV file.", end="")
-try:
-	remove(path.join(cur_dir, "..", "Comments.csv"))
-	print("\r\x1b[32m[*]\x1b[0m Deleting CSV file.")
-except:
-	print("\r\x1b[31m[*]\x1b[0m Could not delete CSV file.")
-
+print("\r\x1b[32m[*]\x1b[0m Saving CSV file.")
 
 print("\x1b[32m[*]\x1b[0m Done.", end="\n\n")
